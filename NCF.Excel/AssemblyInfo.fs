57f986077a15108f74ad54649b4efb670ff57aff
Franco Tiveron <franco.tiveron@gmail.com>
2017-09-01 11:28:20 +1000

    q

--- NCF.Excel/AssemblyInfo.fs
+++ NCF.Excel/AssemblyInfo.fs
@@ -35,7 +35,7 @@ open System.Runtime.InteropServices
 // by using the '*' as shown below:
 [<assembly: AssemblyVersion("0.1.*")>]
 //[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+//[<assembly: AssemblyFileVersion("1.0.0.0")>]
 
 do
     ()
