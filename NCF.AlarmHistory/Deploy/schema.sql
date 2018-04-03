1baf86bf74e0faa6d6f904a23c2671a84ed94359
Franco Tiveron <franco.tiveron@gmail.com>
2018-04-04 09:46:11 +1000

    commit1

--- NCF.AlarmHistory/Deploy/schema.sql
+++ NCF.AlarmHistory/Deploy/schema.sql
@@ -1,7 +1,27 @@
-﻿
-USE [CitectAlarms]
+﻿USE [CitectAlarms]
 GO
+/****** Object:  Table [dbo].[NCFCitectAreas]    Script Date: 4/04/2018 9:06:38 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[NCFCitectAreas](
+	[name] [varchar](50) NULL,
+	[code] [varchar](50) NULL,
+	[desc] [varchar](50) NULL,
+	[zone] [nchar](10) NULL
+) ON [PRIMARY]
 
+GO
+SET ANSI_PADDING ON
+GO
+/****** Object:  Table [dbo].[NCFEvent]    Script Date: 4/04/2018 9:06:38 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[NCFEvent](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[tag] [nvarchar](50) NOT NULL,
@@ -19,7 +39,29 @@ CREATE TABLE [dbo].[NCFEvent](
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 GO
+/****** Object:  Table [dbo].[NCFEventSink]    Script Date: 4/04/2018 9:06:38 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[NCFEventSink](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[tag] [nvarchar](50) NOT NULL,
+	[name] [nvarchar](max) NOT NULL,
+	[desc] [nvarchar](max) NOT NULL,
+	[location] [nvarchar](100) NOT NULL CONSTRAINT [DF_NCFEventSink_location]  DEFAULT (N'cmoc/npm'),
+	[priority] [tinyint] NOT NULL,
+	[timeOn] [datetime] NOT NULL,
+	[timeOff] [datetime] NOT NULL,
+	[duration] [real] NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
+GO
+/****** Object:  Table [dbo].[NCFQuarantinedMessage]    Script Date: 4/04/2018 9:06:38 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[NCFQuarantinedMessage](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[message] [nvarchar](max) NOT NULL,
@@ -31,88 +73,60 @@ CREATE TABLE [dbo].[NCFQuarantinedMessage](
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 GO
-
-
-CREATE UNIQUE NONCLUSTERED INDEX [IX_NCFEvent_tag_timeon_timeoff] ON [dbo].[NCFEvent]
-(
-	[tag] ASC,
-	[timeOn] ASC,
-	[timeOff] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+/****** Object:  StoredProcedure [dbo].[ProcessCitectAlarmMessages]    Script Date: 4/04/2018 9:06:38 AM ******/
+SET ANSI_NULLS OFF
 GO
-
-
-USE [msdb]
+SET QUOTED_IDENTIFIER OFF
 GO
+CREATE PROCEDURE [dbo].[ProcessCitectAlarmMessages]
+WITH EXECUTE AS CALLER
+AS
+EXTERNAL NAME [NCF.AlarmHistory].[NCF.AlarmHistory.Exports].[ProcessCitectAlarmMessages]
+GO
+/****** Object:  StoredProcedure [dbo].[TransferCitectAlarmMessages]    Script Date: 4/04/2018 9:06:38 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[TransferCitectAlarmMessages]
 
-/****** Object:  Job [NCFAlarmHistory]   ******/
-BEGIN TRANSACTION
-DECLARE @ReturnCode INT
-SELECT @ReturnCode = 0
-/****** Object:  JobCategory [Data Collector]   *****/
-IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'Data Collector' AND category_class=1)
+AS
 BEGIN
-EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'Data Collector'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+	SET NOCOUNT ON;
 
+	declare @idMax int
+
+	select @idMax = max(id) from NCFEventSink
+
+	begin transaction;
+		insert into NCFEvent
+			([tag]
+			  ,[name]
+			  ,[desc]
+			  ,[location]
+			  ,[priority]
+			  ,[timeOn]
+			  ,[timeOff]
+			  ,[duration])
+			select max([tag])
+			  ,max([name])
+			  ,max([desc])
+			  ,max([location])
+			  ,max([priority])
+			  ,max([timeOn])
+			  ,max([timeOff])
+			  ,max([duration]) from NCFEventSink t1
+			  where t1.id <= @idMax and not exists(select tag, timeon, timeoff from NCFEvent t2 where t1.tag = t2.tag and t1.timeOn = t2.timeOn and t1.timeoff = t2.timeoff)
+			  group by tag, timeOn, timeOff
+
+
+		delete from NCFEventSink where id <= @idMax
+	commit;
 END
 
-DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'NCFAlarmHistory', 
-		@enabled=1, 
-		@notify_level_eventlog=0, 
-		@notify_level_email=0, 
-		@notify_level_netsend=0, 
-		@notify_level_page=0, 
-		@delete_level=0, 
-		@description=N'No description available.', 
-		@category_name=N'Data Collector', 
-		@owner_login_name=N'NT SERVICE\MSSQLSERVER', @job_id = @jobId OUTPUT
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Run Queue Parser]    Script Date: 3/09/2017 1:09:40 PM ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Run Queue Parser', 
-		@step_id=1, 
-		@cmdexec_success_code=0, 
-		@on_success_action=1, 
-		@on_success_step_id=0, 
-		@on_fail_action=2, 
-		@on_fail_step_id=0, 
-		@retry_attempts=0, 
-		@retry_interval=0, 
-		@os_run_priority=0, @subsystem=N'TSQL', 
-		@command=N'USE [CitectAlarms]
 GO
-
-EXECUTE [dbo].[ProcessCitectAlarmMessages] 
-GO
-
-', 
-		@database_name=N'CitectAlarms', 
-		@flags=0
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'1m', 
-		@enabled=1, 
-		@freq_type=4, 
-		@freq_interval=1, 
-		@freq_subday_type=4, 
-		@freq_subday_interval=1, 
-		@freq_relative_interval=0, 
-		@freq_recurrence_factor=0, 
-		@active_start_date=20170813, 
-		@active_end_date=99991231, 
-		@active_start_time=0, 
-		@active_end_time=235959, 
-		@schedule_uid=N'fa8c1baf-3793-43b5-a6c8-b37332416f49'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
-IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-COMMIT TRANSACTION
-GOTO EndSave
-QuitWithRollback:
-    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
-EndSave:
-
-GO
-
